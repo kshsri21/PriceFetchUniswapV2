e3b844144f22fa921cd8caa272887c1c7c53484d
kshsri21 <95063103+kshsri21@users.noreply.github.com>
2024-07-30 12:54:22 +0530

    Update PriceFetch.js

--- PriceFetch.js
+++ PriceFetch.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const ethers = require('ethers');
-const { erc20ABI, routerABI } = require('./AbiInfo');
-const { routerAddress, fromAddress, toAddress } = require('./AddressList');
+const { erc20ABI, routerABI,factoryABI } = require('./AbiInfo');
+const { routerAddress, fromAddress, toAddress,factoryAddress } = require('./AddressList');
 
 const provider = new ethers.JsonRpcProvider(
   `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
@@ -11,32 +11,55 @@ const createContractInstance = (address, abi, provider) => {
   return new ethers.Contract(address, abi, provider);
 };
 
-const routerInstance = createContractInstance(routerAddress, routerABI, provider);
+
+
+
+const getPairAddress = async () => {
+  try {
+    const factoryInstance = createContractInstance(factoryAddress, factoryABI, provider);
+    const pairAddress = await factoryInstance.getPair(toAddress,fromAddress);
+
+    if (pairAddress === '0x0000000000000000000000000000000000000000') {
+        return false;
+    } else {
+      return true;
+    }
+  } catch (error) {
+    console.error("Error fetching pair address:", error);
+  }
+};
+
 
 const priceFetch = async (humanFormat) => {
   try {
-    const token1 = createContractInstance(fromAddress, erc20ABI, provider);
-    const token2 = createContractInstance(toAddress, erc20ABI, provider);
+    if(await getPairAddress()){
+      const token1 = createContractInstance(fromAddress, erc20ABI, provider);
+      const token2 = createContractInstance(toAddress, erc20ABI, provider);
+  
+      const [decimal1, decimal2] = await Promise.all([
+        token1.decimals(),
+        token2.decimals(),
+      ]);
+  
+      const amountIn = ethers.parseUnits(humanFormat, decimal1); // Ensure proper parsing
+      const routerInstance = createContractInstance(routerAddress, routerABI, provider);
+      const amountsOut = await routerInstance.getAmountsOut(amountIn, [
+        fromAddress,
+        toAddress,
+      ]);
+  
+      const humanOutput = ethers.formatUnits(amountsOut[1], decimal2); // Ensure proper formatting
+  
+      console.log(`This is the number of DAI: ${humanOutput}`);
+    } else{
+      console.log("Pair does not exist")
+    }
 
-    const [decimal1, decimal2] = await Promise.all([
-      token1.decimals(),
-      token2.decimals(),
-    ]);
-
-    const amountIn = ethers.parseUnits(humanFormat, decimal1).toString();
-
-    const amountsOut = await routerInstance.getAmountsOut(amountIn, [
-      fromAddress,
-      toAddress,
-    ]);
-
-    const humanOutput = ethers.formatUnits(amountsOut[1].toString(), decimal2);
-
-    console.log(`This is the number of DAI: ${humanOutput}`);
   } catch (error) {
     console.error('Error fetching price:', error);
   }
 };
+
 
 // Example usage
 const humanFormat = '1';
